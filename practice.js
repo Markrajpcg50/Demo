b7af8ebd58f37fc926a8093e673fa5f4d2fd91e8
Markrajpcg50 <112145938+Markrajpcg50@users.noreply.github.com>
2022-11-27 14:35:51 +0530

    basics of javascript

--- practice.js
+++ practice.js
@@ -1,2 +1,27 @@
 console.log("Hello World")
 var a=10;
+var b=20;
+b="Mohan";
+console.log(b);
+
+function addition(a,b){
+    return a+b;
+}
+
+var myObj={
+    "id":1,
+    "name":"Harish"
+};
+
+console.log(myObj);
+
+myObj.nickName="yenne Gadi";
+console.log(myObj);
+var myArray=[];
+myArray[0]=1;
+myArray[1]="Jagath";
+console.log(myArray);
+myArray[2]=true;
+myArray[0]="amogha";
+console.log(myArray);
+
